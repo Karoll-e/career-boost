731ae8684e919983c40b2f76dca94be33b39936a
Karoll Escalante <karollescalante02@gmail.com>
2025-06-18 17:14:15 -0500

    Implement routing and layout for the application

--- frontend/career-boost/src/App.jsx
+++ frontend/career-boost/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-amber-500">
+      <Router>
+        {/* Default Route */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
 
-export default App
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/interview-prep/:sessionId"
+            element={<InterviewPrep />}
+          />
+        </Routes>
+      </Router>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
