5b592b1dd4a90ef28fea3ce271b473a9dc597c80
Karoll Escalante <karollescalante02@gmail.com>
2025-06-18 17:11:52 -0500

    Add initial components and utility functions for LandingPage, SignUp, and InterviewPrep

---
+++ frontend/career-boost/src/pages/LandingPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+// import Login from "./Auth/Login";
+// import SignUp from "./Auth/SignUp";
+// import { HERO_IMG } from "../assets/hero.jpg";
+// import { APP_FEATURE } from "../utils/data";
+import Navbar from "../components/Navbar";
+
+const LandingPage = () => {
+  // const navigate = useNavigate();
+
+  // const [openAuthModal, setOpenAuthModal] = useState(false);
+  // const [currentPage, setCurrentPage] = useState("login");
+
+  // const handleCTA = () => {};
+
+  return (
+    <div className="">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default LandingPage;
