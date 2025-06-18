d24064a4728bbeaf2961cd8d61c377d157d26722
Karoll Escalante <karollescalante02@gmail.com>
2025-06-18 17:14:26 -0500

    Add Navbar and Button components with styling and functionality

---
+++ frontend/career-boost/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+import Logo from "../assets/logo.png";
+import { SunIcon } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <div className="min-h-screen bg-muted">
+      <nav className="fixed top-6 inset-x-4 h-16 bg-background border dark:border-slate-700/70 max-w-screen-sm mx-auto rounded-xl">
+        <div className="h-full flex items-center justify-between mx-auto px-4">
+          <div className="flex items-center gap-2 md:gap-6 cursor-pointer">
+            <img src={Logo} alt="Logo" className="shrink-0" width={150} />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Button size="icon" variant="outline">
+              <SunIcon />
+            </Button>
+            <Button
+              variant="outline"
+              className="hidden sm:inline-flex"
+            >
+              Sign In
+            </Button>
+            <Button className="">Get Started</Button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
