f3ee883a90043b4eef8a8755b3682aa52aeeae9f
Karoll Escalante <karollescalante02@gmail.com>
2025-06-21 21:23:45 -0500

    Add database connection setup and define authentication routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -14,8 +15,13 @@ app.use(
   })
 );
 
+connectDB()
+
 // Middleware
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
