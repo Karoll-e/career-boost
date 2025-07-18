7d1e9ca9392c82eb64a534dd84b8d686e488fc6a
Karoll Escalante <karollescalante02@gmail.com>
2025-07-17 20:34:37 -0500

    Refactor App component to integrate ProtectedRoute for Dashboard and InterviewPrep, and add AppSidebar component for enhanced navigation

--- frontend/career-boost/src/App.jsx
+++ frontend/career-boost/src/App.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import UserProvider from "./context/userContext";
 import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/Home/Dashboard";
+// import Dashboard from "./pages/Home/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
+ import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
@@ -15,10 +17,21 @@ const App = () => {
             {/* Default Route */}
             <Route path="/" element={<LandingPage />} />
 
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route 
+              path="/dashboard" 
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              } 
+            />
             <Route
               path="/interview-prep/:sessionId"
-              element={<InterviewPrep />}
+              element={
+                <ProtectedRoute>
+                  <InterviewPrep />
+                </ProtectedRoute>
+              }
             />
           </Routes>
         </Router>
