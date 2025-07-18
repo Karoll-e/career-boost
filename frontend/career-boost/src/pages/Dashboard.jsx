0c33373a25a0e05224d1758636ee566ef93dd2c0
Karoll Escalante <karollescalante02@gmail.com>
2025-07-17 20:34:25 -0500

    Add Dashboard and DashboardHeader components for enhanced user interface and statistics display

---
+++ frontend/career-boost/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "../components/layouts/DashboardLayout";
+import DashboardHeader from "./InterviewPrep/components/dashboard/DashboardHeader";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+function Dashboard() {
+  return (
+    <DashboardLayout>
+      <DashboardHeader />
+    </DashboardLayout>
+  );
+}
+
+export default Dashboard;
